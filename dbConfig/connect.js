27419b04127bbfc9104768d291d92a59aa9b97d7
agiskorska <skorskaga@gmail.com>
2022-08-16 21:27:14 +0100

    Add dotenv npm

--- dbConfig/connect.js
+++ dbConfig/connect.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-require("../.env").config();
+require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
 const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
