33144725c54a0491953b30b95fdf0d3d5225728f
agiskorska <skorskaga@gmail.com>
2022-08-16 21:41:45 +0100

    config for heroku

--- dbConfig/connect.js
+++ dbConfig/connect.js
@@ -1,5 +1,7 @@
-const { Pool } = require("pg");
+const { Client } = require("pg");
 
-const pool = new Pool();
+const pool = new Client({
+    connectionString: 'postgres://sbjfbhfzgoiswk:fb717eea0bc01df8c6823941d0e76e784bc54e49a58b787720cac66eafb70b59@ec2-44-195-100-240.compute-1.amazonaws.com:5432/d9qi67n5l29vl5'
+});
 
 module.exports = pool;
