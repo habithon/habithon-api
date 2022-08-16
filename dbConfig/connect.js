dddbde4cd75c33d1e60755e93fd0e7924bfc5ce7
agiskorska <skorskaga@gmail.com>
2022-08-16 21:21:09 +0100

    Set up files for heroku

--- dbConfig/connect.js
+++ dbConfig/connect.js
@@ -1,5 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = new Pool();
-
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 module.exports = pool;
