004361ee59781d24a565d91f5b25682a43bc2092
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-18 09:22:20 +0100

    New test seed

--- tests/integration/test_seeds.sql
+++ tests/integration/test_seeds.sql
@@ -1,12 +1,34 @@
-INSERT INTO user_account (username, password)
-VALUES (
-    'ladybird', '$2a$12$5QAvn3Hp/949tnXHWg/zBuxmHkWaSUm0r9C6gooCUxdx4s9ZxTo1K'
+DROP TABLE IF EXISTS habit;
+DROP TABLE IF EXISTS user_account;
+
+CREATE TABLE user_account (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    password CHAR(60) NOT NULL,
+    full_name VARCHAR(100),
+    email VARCHAR(50) 
 );
 
-INSERT INTO habit (user_id, habit, streak, frequency, last_completed)
+CREATE TABLE habit (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES user_account(id),
+    image_url VARCHAR(100),
+    habit VARCHAR(50) NOT NULL,
+    streak INT DEFAULT 0,
+    frequency VARCHAR(20) NOT NULL,
+    last_completed TIMESTAMPTZ
+);
+
+
+INSERT INTO user_account (username, password)
+VALUES 
+    ('ladybird', '$2a$12$5QAvn3Hp/949tnXHWg/zBuxmHkWaSUm0r9C6gooCUxdx4s9ZxTo1K'),
+    ('lordbird', '$2a$12$5QAvn3Hp/949tnXHWg/zBuxmHkWaSUm0r9C6gooCUxdx4s9ZxTo1K');
+
+INSERT INTO habit (user_id, image_url, habit, streak, frequency, last_completed)
 VALUES
-(1, 'sleep', 10, 'daily', '2022-08-15'),
-(1, 'run', 10, 'daily', '2022-08-15'),
-(1, 'eat', 10, 'daily', '2022-08-15'),
-(2, 'sleep', 10, 'daily', '2022-08-15'),
-(2, 'run', 10, 'daily', '2022-08-15');
+(1, 'images/goals-water.jpg','water', 10, 'weekly', '2022-08-15'),
+(1, 'images/goals-exercise.jpg','exercise', 10, 'daily', '2022-08-15'),
+(1, 'images/goals-reading.jpg','read', 10, 'daily', '2022-08-15'),
+(2, 'images/goals-sleep.jpg','sleep', 10, 'daily', '2022-08-15'),
+(2, 'images/goals-exercise.jpg','exercise', 10, 'daily', '2022-08-15');
