6d807a4296dc9ad1a06c2fb3cd1792c66231cb4e
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-18 15:38:34 +0100

    Update create func

--- models/Goal.js
+++ models/Goal.js
@@ -105,7 +105,7 @@ module.exports = class Goal {
     return new Promise(async (resolve, reject) => {
       try {
         const goalData = await db.query(
-          "INSERT INTO habit (user_id, habit, streak, frequency, last_completed) VALUES ($1, $2, $3, $4, NOW() - INTERVAL 1 DAY) RETURNING *;",
+          "INSERT INTO habit (user_id, habit, streak, frequency, last_completed) VALUES ($1, $2, $3, $4, '0000-00-00') RETURNING *;",
           [data.user_id, data.habit, 0, data.frequency]
         );
         const habit = new Goal(goalData.rows[0]);
